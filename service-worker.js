bd8a68bcb7b8246ba84dd6e1b4a776f805f0574d
soportebdinformacion-dev <soporte.bd.informacion@gmail.com>
2026-05-04 11:08:32 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,17 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("app").then(cache => {
-      return cache.addAll(["/", "/index.html", "/app.js"]);
-    })
-  );
+const CACHE_NAME = 'huarmey-v1';
+const ASSETS = [
+  './',
+  './tu_archivo.html', // Cambia esto al nombre real de tu archivo
+  './ICONO HUARMEY.png',
+  'https://unpkg.com/dexie@3.2.0/dist/dexie.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
